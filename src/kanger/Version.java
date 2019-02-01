6f66b9788782e2eb378290d4e057176189624f84
Dmitry G. Quznetsov <murray@letit.ru>
2019-02-01 23:31:35 +0300

    Разделение Values на системные и штатные

--- src/kanger/Version.java
+++ src/kanger/Version.java
@@ -16,8 +16,8 @@ public abstract class Version {
 
     public static final int VERSION = 3;
     public static final int RELEASE = 2;
-    public static final String REVISION = "259";
-    public static final String DATE = "2019-02-01_16:32:30";
+    public static final String REVISION = "261";
+    public static final String DATE = "2019-02-01_23:21:02";
     public static final int YEAR = getYear(parseDate(DATE));
     public static final int VERSION_CODE = ((VERSION & 0xFF) << 8) | (RELEASE & 0xFF);
     public static final String VERSION_S = String.format("%d.%d.%s", VERSION, RELEASE, REVISION);
