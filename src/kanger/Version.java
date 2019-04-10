f222760ab3745fc623bc9211e5815975aa5b11ba
Dmitry G. Qusnetsov <murray@letit.ru>
2019-04-10 23:07:59 +0300

    Версия

--- src/kanger/Version.java
+++ src/kanger/Version.java
@@ -16,8 +16,8 @@ public abstract class Version {
 
     public static final int VERSION = 3;
     public static final int RELEASE = 2;
-    public static final String REVISION = "305";
-    public static final String DATE = "2019-02-12_17:59:58";
+    public static final String REVISION = "306";
+    public static final String DATE = "2019-03-24_16:13:54";
     public static final int YEAR = getYear(parseDate(DATE));
     public static final int VERSION_CODE = ((VERSION & 0xFF) << 8) | (RELEASE & 0xFF);
     public static final String VERSION_S = String.format("%d.%d.%s", VERSION, RELEASE, REVISION);
