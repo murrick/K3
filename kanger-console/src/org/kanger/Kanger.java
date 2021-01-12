ad2d5ed7a0d485623faff172def2c6eb1a369dd2
Dmitry G. Qusnetsov <murray@kanger.org>
2021-01-13 00:02:06 +0300

    Пользовательский вход по логину/паролю

--- kanger-console/src/org/kanger/Kanger.java
+++ kanger-console/src/org/kanger/Kanger.java
@@ -1,9 +1,10 @@
 package org.kanger;
 
-import org.kanger.interfaces.IData;
 import org.kanger.interfaces.IUser;
-import org.kanger.storage.DB;
-import org.kanger.udf.UDF;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 /**
@@ -13,6 +14,26 @@ public class Kanger {
 
 
     public static void main(String[] args) throws Exception {
+
+        String login = null;
+        String password = null;
+
+        for (int i = 0; i < args.length; ++i) {
+            if ((args[i].equals("--user") || args[i].equals("-U")) && args.length > i + 1) {
+                login = args[++i];
+            } else if ((args[i].equals("--password") || args[i].equals("-P")) && args.length > i + 1) {
+                password = args[++i];
+            }
+        }
+
+        if (login != null) {
+            try {
+                User.createUser(login, password);
+                System.exit(1);
+            } catch (Exception ex) {
+
+            }
+        }
 
 //        IData db = null;
 //        Class udf = null;
@@ -56,12 +77,30 @@ public class Kanger {
 //            }
 //        }
 
+        Kanger k = new Kanger();
         Screen.showCopyrigt();
-        String login = System.console().readLine("login: ");
-        String password = new String(System.console().readPassword("password: "));
+        login = k.readLine("login: ");
+        password = new String(k.readPassword("password: "));
 
         IUser user = new User(login, password);
 
         Screen.session(user);
+    }
+
+    private String readLine(String format, Object... args) throws IOException {
+        if (System.console() != null) {
+            return System.console().readLine(format, args);
+        }
+        System.out.print(String.format(format, args));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                System.in));
+        return reader.readLine();
+    }
+
+    private char[] readPassword(String format, Object... args)
+            throws IOException {
+        if (System.console() != null)
+            return System.console().readPassword(format, args);
+        return this.readLine(format, args).toCharArray();
     }
 }
--- kanger/src/org/kanger/Version.java
+++ kanger/src/org/kanger/Version.java
@@ -15,8 +15,8 @@ public abstract class Version {
 
     public static final int VERSION = 3;
     public static final int RELEASE = 2;
-    public static final String REVISION = "5138";
-    public static final String DATE = "2021-01-12_18:04:01";
+    public static final String REVISION = "5153";
+    public static final String DATE = "2021-01-12_23:56:12";
     public static final int YEAR = getYear(parseDate(DATE));
     public static final int VERSION_CODE = ((VERSION & 0xFF) << 8) | (RELEASE & 0xFF);
     public static final String VERSION_S = String.format("%d.%d.%s", VERSION, RELEASE, REVISION);
