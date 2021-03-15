fd6b388d73b622c5ac493c2e261a92a67a5ae876
Dmitry G. Qusnetsov <murray@kanger.org>
2021-03-15 15:34:00 +0300

    Поправил косяк с переоткрытием базы

--- kanger/src/org/kanger/Version.java
+++ kanger/src/org/kanger/Version.java
@@ -36,8 +36,8 @@ public abstract class Version {
 
     public static final int VERSION = 3;
     public static final int RELEASE = 2;
-    public static final String REVISION = "6518";
-    public static final String DATE = "2021-03-15_15:11:34";
+    public static final String REVISION = "6519";
+    public static final String DATE = "2021-03-15_15:14:38";
     public static final int YEAR = getYear(parseDate(DATE));
     public static final int VERSION_CODE = ((VERSION & 0xFF) << 8) | (RELEASE & 0xFF);
     public static final String VERSION_S = String.format("%d.%d.%s", VERSION, RELEASE, REVISION);
