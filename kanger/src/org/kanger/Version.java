7722c421856da6251b159876d3c709245eb8d246
Dmitry G. Qusnetsov <murray@kanger.org>
2021-03-30 16:37:26 +0300

    В хелпе небыло транзакций. Поправил сбор гирлтез во время линка. Задачка про кота

--- kanger/src/org/kanger/Version.java
+++ kanger/src/org/kanger/Version.java
@@ -36,8 +36,8 @@ public abstract class Version {
 
     public static final int VERSION = 3;
     public static final int RELEASE = 2;
-    public static final String REVISION = "6940";
-    public static final String DATE = "2021-03-30_15:46:59";
+    public static final String REVISION = "6941";
+    public static final String DATE = "2021-03-30_16:09:47";
     public static final int YEAR = getYear(parseDate(DATE));
     public static final int VERSION_CODE = ((VERSION & 0xFF) << 8) | (RELEASE & 0xFF);
     public static final String VERSION_S = String.format("%d.%d.%s", VERSION, RELEASE, REVISION);
