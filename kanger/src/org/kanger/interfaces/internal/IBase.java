5d7422df818e7740eb87a8368292eb677bdaa280
Дмитрий Г. Кузнецов <murray@letit.ru>
2026-07-29 15:48:52 +0300

    P2: expose backend-neutral cache metrics

--- kanger/src/org/kanger/interfaces/internal/IBase.java
+++ kanger/src/org/kanger/interfaces/internal/IBase.java
@@ -10,17 +10,16 @@
  *  sell copies of the Software, and to permit persons to whom the Software is
  *  furnished to do so, subject to the following conditions:
  *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- *  IN THE SOFTWARE.
- *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  */
 
 package org.kanger.interfaces.internal;
@@ -61,6 +60,30 @@ public interface IBase {
     long getUsedCacheSize();
 
     long getMaxCacheSize();
+
+    /**
+     * Optional cache telemetry. Default methods keep historical/pluggable
+     * storage engines source-compatible until they opt in.
+     */
+    default long getCacheHits() {
+        return -1L;
+    }
+
+    default long getCacheMisses() {
+        return -1L;
+    }
+
+    default long getCacheEvictions() {
+        return -1L;
+    }
+
+    default long getCachedEntryCount() {
+        return -1L;
+    }
+
+    default boolean isCacheEnabled() {
+        return getMaxCacheSize() > 0L;
+    }
 
     long lastId();
 
