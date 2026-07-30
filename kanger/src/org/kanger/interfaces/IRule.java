1cba1fdedb262a1588a2ad678b483cb981967d4a
Дмитрий Г. Кузнецов <murray@letit.ru>
2026-07-30 15:54:27 +0300

    Expose context-aware rule rendering in API

--- kanger/src/org/kanger/interfaces/IRule.java
+++ kanger/src/org/kanger/interfaces/IRule.java
@@ -4,22 +4,22 @@
  * Copyright (c) 2021 Dmitry G. Quznetsov
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to
- *  deal in the Software without restriction, including without limitation the
- *  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- *  sell copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- *  IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  *
  */
 
@@ -51,6 +51,18 @@ public interface IRule {
      * @throws Exception
      */
     String getOrigin() throws Exception;
+
+    /**
+     * Получить фактическое текстовое представление правила в указанном
+     * транзакционном контексте. Для утверждения представление строится из
+     * дерева правила и поэтому содержит уже материализованные параметры,
+     * включая лениво загруженные из хранилища значения. Исходный текст,
+     * возвращаемый {@link #getOrigin()}, при этом не изменяется.
+     *
+     * @param mind транзакционный контекст для разрешения ленивых ссылок.
+     * @return фактическое текстовое представление правила.
+     */
+    String toString(IMind mind);
 
     /**
      * Признак того что правило является производным утверждением,
