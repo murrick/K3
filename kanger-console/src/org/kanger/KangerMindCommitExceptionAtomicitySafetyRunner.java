cd812c1aa1af05db3052dcd2e78fa23c1c712e49
Дмитрий Г. Кузнецов <murray@letit.ru>
2026-07-31 13:59:32 +0300

    Normalize exception atomicity runner user identity

--- kanger-console/src/org/kanger/KangerMindCommitExceptionAtomicitySafetyRunner.java
+++ kanger-console/src/org/kanger/KangerMindCommitExceptionAtomicitySafetyRunner.java
@@ -31,8 +31,9 @@ public final class KangerMindCommitExceptionAtomicitySafetyRunner {
     public static void main(String[] args) {
         int exitCode = 1;
         try {
-            IUser user = UserFactory.createUser("mind-commit-exception-atomicity-" + + System.nanoTime(),
-                    "mind-commit-exception-atomicity");
+            String userName =
+                    "mind-commit-exception-atomicity-" + System.nanoTime();
+            IUser user = UserFactory.createUser(userName, userName);
             new UDF().init(user);
             new DB().init(user);
 
