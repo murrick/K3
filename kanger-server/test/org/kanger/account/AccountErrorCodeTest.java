ecdd1350cb5e1940d285b3696f7254e21fca8739
Дмитрий Г. Кузнецов <murray@letit.ru>
2026-08-04 14:11:57 +0300

    Test immutable account login error code

--- kanger-server/test/org/kanger/account/AccountErrorCodeTest.java
+++ kanger-server/test/org/kanger/account/AccountErrorCodeTest.java
@@ -30,6 +30,7 @@ class AccountErrorCodeTest {
                 "CONFIRMATION_TOKEN_EXPIRED",
                 "EMAIL_ALREADY_USED",
                 "VERIFIED_EMAIL_IMMUTABLE",
+                "ACCOUNT_LOGIN_IMMUTABLE",
                 "LOGIN_ALREADY_USED",
                 "RESEND_RATE_LIMITED",
                 "MAIL_DELIVERY_UNAVAILABLE"), codes);
