01dd2520174ba1aa33abb10d8a3aad49865ca6af
Дмитрий Г. Кузнецов <murray@letit.ru>
2026-08-15 14:26:13 +0300

    test(server): qualify declarative editor boundary

---
+++ kanger-server/test/org/kanger/DeclarativeSourceBoundaryTest.java
@@ -0,0 +1,33 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Dmitry G. Quznetsov
+ */
+package org.kanger;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class DeclarativeSourceBoundaryTest {
+
+    @Test
+    void rejectsQueryStatementsAtAnyStatementBoundary() {
+        assertNotNull(DeclarativeSourceBoundary.rejection("?x;"));
+        assertNotNull(DeclarativeSourceBoundary.rejection("!base;\n  ?x;"));
+        assertNotNull(DeclarativeSourceBoundary.rejection("/* prefix */ ?x;"));
+    }
+
+    @Test
+    void ignoresQuestionMarksInsideCommentsAndQuotedText() {
+        assertNull(DeclarativeSourceBoundary.rejection(
+                "// ? not a statement\n!base;"));
+        assertNull(DeclarativeSourceBoundary.rejection(
+                "/* ? not a statement */\n!base;"));
+        assertNull(DeclarativeSourceBoundary.rejection(
+                "!text(\"?\");"));
+        assertNull(DeclarativeSourceBoundary.rejection(
+                "!text('?');"));
+    }
+}
